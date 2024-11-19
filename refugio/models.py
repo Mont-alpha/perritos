499e549a0fa1a47f063de1c90275ec906b79c7c1
Mont-alpha <cristian.montiel@usm.cl>
2024-11-19 19:50:10 -0300

    cambios pedidos

--- refugio/models.py
+++ refugio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Perrito(models.Model):
@@ -26,6 +27,11 @@ class Perrito(models.Model):
     es_adopcion_doble = models.OneToOneField('Perrito', on_delete=models.DO_NOTHING,null=True, blank=True)
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='perritos/')
+    
+    def delete(self, *args, **kwargs):
+        if self.imagen and os.path.isfile(self.imagen.path):
+            os.remove(self.imagen.path)  # Eliminar la imagen del sistema
+        super().delete(*args, **kwargs)  # Eliminar el objeto de la base de datos
     
     def __str__(self):
         return f"{self.nombre} - {self.sexo} - {self.tamano}"
